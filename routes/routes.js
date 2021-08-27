3969029c31f59f15159de8b87180926bb8ea9120
Kumaran <40437078+kumaranbcg@users.noreply.github.com>
2021-08-27 17:07:56 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 router.get("/myservice-1", (err, res) => {
-    return res.send("I am Service 1");
+    return res.send("I am Service two");
 });
 router.get("/myservice-1/hello", (err, res) => {
     return res.send("Saying hello...");
