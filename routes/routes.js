700b3ac9075b19a56b525da00f656f2d0d6de4e0
Kumaran <40437078+kumaranbcg@users.noreply.github.com>
2021-09-02 20:39:26 +0530

    code merge

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const router = express.Router();
 router.get("/healthcheck", (err, res) => {
-    return res.send(`
-        I am Healthcheck Service 1 
-        Node env: ${process.env.NODE_ENV}
-        DB username: ${process.env.DB_USERNAME}
-        DB password: ${process.env.DB_PASSWORD}
-        `);
+    return res.send("I am Healthcheck Service 1");
 });
 router.get("/myservice-1", (err, res) => {
-    return res.send("I am Service two");
+    return res.send("I am Service 1");
 });
 router.get("/myservice-1/hello", (err, res) => {
     return res.send("Saying hello...");
